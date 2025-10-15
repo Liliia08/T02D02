14e8fca4c9531ec57f6866e90e90962c2c550520
Liliia08 <125467004+Liliia08@users.noreply.github.com>
2025-10-15 16:01:53 +0300

    Задание 2:Добавьте в модель «Заказ» новое поле — дату заказа.

--- src/CampusBenchApi/CampusBenchApi/Migrations/20251015122916_AddOrderDateToOrder.Designer.cs
+++ src/CampusBenchApi/CampusBenchApi/Migrations/20251015122916_AddOrderDateToOrder.Designer.cs
@@ -2,6 +2,7 @@
 using CampusBenchApi.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CampusBenchApi.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251015122916_AddOrderDateToOrder")]
+    partial class AddOrderDateToOrder
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
